6c325bae245bcf5f1b10aa851cfd8e7eb58c89eb
huang.xinghui <huang.x.hui@gmail.com>
2015-09-15 22:16:47 +0800

    add phantomjs proxy setting

--- cli.js
+++ cli.js
@@ -7,6 +7,7 @@ program
     .version('1.0.0')
     .option('-a, --address [value]', 'html address')
     .option('-o, --output [value]', 'output file name', 'output.png')
+    .option('-p, --proxy', 'use socks5 proxy')
     .parse(process.argv);
 
-screenCapture(program.address, program.output);
+screenCapture(program.address, program.output, program.proxy);
