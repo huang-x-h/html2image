6a372aaaf7cdeb32b773603d81c39d88888747e7
huang.xinghui <huang.x.hui@gmail.com>
2015-09-16 14:36:54 +0800

    change default format value to pdf

--- cli.js
+++ cli.js
@@ -6,7 +6,7 @@ var screenCapture = require('./lib/screencapture');
 program
     .version('1.0.0')
     .option('-a, --address [value]', 'html address')
-    .option('-f, --format [value]', 'output file format', 'png')
+    .option('-f, --format [value]', 'output file format', 'pdf')
     .option('-p, --proxy', 'use socks5 proxy')
     .parse(process.argv);
 
