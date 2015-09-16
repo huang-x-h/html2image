5cbacf84d6a3cd4381c8b3b1500f530ee9fa8658
huang.xinghui <huang.x.hui@gmail.com>
2015-09-16 11:04:47 +0800

    change output file name use html title

--- cli.js
+++ cli.js
@@ -6,8 +6,8 @@ var screenCapture = require('./lib/screencapture');
 program
     .version('1.0.0')
     .option('-a, --address [value]', 'html address')
-    .option('-o, --output [value]', 'output file name', 'output.png')
+    .option('-f, --format [value]', 'output file format', 'png')
     .option('-p, --proxy', 'use socks5 proxy')
     .parse(process.argv);
 
-screenCapture(program.address, program.output, program.proxy);
+screenCapture(program.address, program.format, program.proxy);
--- phantom/screencapture.js
+++ phantom/screencapture.js
@@ -5,7 +5,7 @@
 // copy from https://github.com/ariya/phantomjs/blob/master/examples/rasterize.js
 var page = require('webpage').create(),
     system = require('system'),
-    address, output, size;
+    address, output, format, size;
 
 if (system.args.length < 3 || system.args.length > 5) {
     console.log('Usage: rasterize.js URL filename [paperwidth*paperheight|paperformat] [zoom]');
@@ -15,35 +15,21 @@ if (system.args.length < 3 || system.args.length > 5) {
     phantom.exit(1);
 } else {
     address = system.args[1];
-    output = system.args[2];
-    page.viewportSize = { width: 1024, height: 600 };
-    if (system.args.length > 3 && system.args[2].substr(-4) === ".pdf") {
-        size = system.args[3].split('*');
-        page.paperSize = size.length === 2 ? { width: size[0], height: size[1], margin: '0px' }
-            : { format: system.args[3], orientation: 'portrait', margin: '1cm' };
-    } else if (system.args.length > 3 && system.args[3].substr(-2) === "px") {
-        size = system.args[3].split('*');
-        if (size.length === 2) {
-            pageWidth = parseInt(size[0], 10);
-            pageHeight = parseInt(size[1], 10);
-            page.viewportSize = { width: pageWidth, height: pageHeight };
-            page.clipRect = { top: 0, left: 0, width: pageWidth, height: pageHeight };
-        } else {
-            console.log("size:", system.args[3]);
-            pageWidth = parseInt(system.args[3], 10);
-            pageHeight = parseInt(pageWidth * 3/4, 10); // it's as good an assumption as any
-            console.log ("pageHeight:",pageHeight);
-            page.viewportSize = { width: pageWidth, height: pageHeight };
-        }
+    format = system.args[2];
+
+    if (format === 'padf') {
+        page.paperSize = { format: 'A4', orientation: 'portrait', margin: '1cm' }
     }
-    if (system.args.length > 4) {
-        page.zoomFactor = system.args[4];
-    }
+
     page.open(address, function (status) {
         if (status !== 'success') {
             console.log('Unable to load the address!');
             phantom.exit(1);
         } else {
+            output = page.evaluate(function () {
+                return document.querySelector('title').textContent;
+            }) + '.' + format;
+
             window.setTimeout(function () {
                 page.render(output);
                 phantom.exit();
